c6c3305a29fad5e76bea052a7081f00a3d69976b
Philip <Philip@s099298t.local>
2015-10-19 18:47:22 +0200

    Merge origin/master
    Conflicts:
    	TwitTwins/src/Main/TwitMain.java

--- TwitTwins/src/Main/TwitMain.java
+++ TwitTwins/src/Main/TwitMain.java
@@ -149,13 +149,14 @@ public class TwitMain {
         
         // Sort the scores list in ascending order of scores (and their corresponding users)
         System.out.println("-------- VSR Ranking results --------");
+        System.out.println("Query: " + query);
         Collections.sort(scores);
         Collections.reverse(scores); // Changes the list to an ascending order.
         int rank = 0;
         for (Object o : scores) {
            Score s = (Score) o;
            rank++;
-           //System.out.println("Ranked: " + s.getName()+ "with score: " +"\t"+ s.getScore()  ); 
+           //System.out.println("Ranked: " + s.getName()+ "with score: " +"\t"+ s.getScore()  );
            System.out.format("#%d: \t %-20s \t (CosineScore: %f)%n", rank, s.getName(), s.getScore());
         }
         
@@ -194,27 +195,3 @@ public class TwitMain {
     
 }
 
-
-    // Used for sorting on cosine scores of users
-//    class Score implements Comparable<Score> {
-//        double score;
-//        String name;
-//
-//        public Score(double score, String name) {
-//        this.score = score;
-//        this.name = name;
-//        }
-//
-//        @Override
-//        public int compareTo(Score o) {
-//            return score < o.score ? -1 : score > o.score ? 1 : 0;
-//        }
-//            
-//        public double getScore() {
-//            return score;
-//        }
-//            
-//        public String getName() {
-//            return name;
-//        }
-//    }
--- TwitTwins/src/Main/VectorIR.java
+++ TwitTwins/src/Main/VectorIR.java
@@ -32,12 +32,18 @@ public class VectorIR {
     
     static double cosine_similarity(Map<String, Double> v1, Map<String, Double> v2) {
             Set<String> both = Sets.newHashSet(v1.keySet());
+            double score;
             both.retainAll(v2.keySet());
             double sclar = 0, norm1 = 0, norm2 = 0;
             for (String k : both) sclar += v1.get(k) * v2.get(k);
             for (String k : v1.keySet()) norm1 += v1.get(k) * v1.get(k);
             for (String k : v2.keySet()) norm2 += v2.get(k) * v2.get(k);
-            return sclar / Math.sqrt(norm1 * norm2);
+            score = sclar / Math.sqrt(norm1 * norm2);
+            
+            if ( Double.isNaN(score) ) {
+                return 0.0;
+            }
+            else return score;
     }
     
     public void CompareVectors () {
