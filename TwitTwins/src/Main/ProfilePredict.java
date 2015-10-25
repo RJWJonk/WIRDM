426b8b65e08547177ece0d2d32f674bb141b9a5d
s119503 <s119503@S119503.campus.tue.nl>
2015-10-25 17:58:42 +0100

    More cleaned code

--- TwitTwins/src/Main/ProfilePredict.java
+++ TwitTwins/src/Main/ProfilePredict.java
@@ -9,36 +9,15 @@ package Main;
 import com.facepp.error.FaceppParseException;
 import com.facepp.http.PostParameters;
 import com.facepp.result.FaceppResult;
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
 
 /**
  *
- * @author chung_000
+ * @author s119503
  */
 public class ProfilePredict {
     
-        private final static Twitter twitter = authenticate();
-	public static String twitterUser = "SCGOP"; //some given (real) twitter user
-        
-    private static Twitter authenticate() {
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("b0DV73vfaiAinFwkc0BsaGWRi")
-                .setOAuthConsumerSecret("lM35xfcnaJQDaUuoZsB749bNv1GbA8dbItDH9VlglmbAefUZn5")
-                .setOAuthAccessToken("2387531042-QRMVloVxBoNYntQMvKs7dZHN8ybe3ciwS34JzBz")
-                .setOAuthAccessTokenSecret("t3RtJdrcYaf9EfDRxVgD9vO4FXYh8vIv0XVfC1D4ojkF8");
-        TwitterFactory tf = new TwitterFactory(cb.build());
-        return tf.getInstance();
-    }
-    
     public String getGender(String URL) throws FaceppParseException {
-        HttpRequests httpRequests = new HttpRequests("25ad86cdad3ea4ac04204464a210058d", "phbtAB6cTT4LcArmskLCin2O5v6hDZfa");
-        PostParameters postParameters;
-        postParameters = new PostParameters().setUrl(URL).setAttribute("all");
-        httpRequests.detectionDetect(postParameters);
-        FaceppResult result = httpRequests.detectionDetect(postParameters);
+        FaceppResult result = returnResult(URL);
         String s="n.a.";
         String r=""+result.get("face");
         if (!r.contains("[]")){
@@ -48,11 +27,7 @@ public class ProfilePredict {
     }
     
     public int getAge(String URL) throws FaceppParseException {
-        HttpRequests httpRequests = new HttpRequests("25ad86cdad3ea4ac04204464a210058d", "phbtAB6cTT4LcArmskLCin2O5v6hDZfa");
-        PostParameters postParameters;
-        postParameters = new PostParameters().setUrl(URL).setAttribute("all");
-        httpRequests.detectionDetect(postParameters);
-        FaceppResult result = httpRequests.detectionDetect(postParameters);
+        FaceppResult result = returnResult(URL);
         int a=-1;
         String r=""+result.get("face");
         if (!r.contains("[]")){
@@ -61,5 +36,14 @@ public class ProfilePredict {
             a = Integer.parseInt(s);
         }
         return a;
+    }
+    
+    private FaceppResult returnResult(String URL) throws FaceppParseException{
+        HttpRequests httpRequests = new HttpRequests("25ad86cdad3ea4ac04204464a210058d", "phbtAB6cTT4LcArmskLCin2O5v6hDZfa");
+        PostParameters postParameters;
+        postParameters = new PostParameters().setUrl(URL).setAttribute("all");
+        httpRequests.detectionDetect(postParameters);
+        FaceppResult result = httpRequests.detectionDetect(postParameters);
+        return result;
     }
 }
